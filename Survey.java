b465af88e2927e0a682f1ac98e6a20a1ed3fb6c3
randy <randyr_1993@yahoo.com>
2022-09-21 23:35:05 -0400

    Completed dynamic Survey Application

--- Survey.java
+++ Survey.java
@@ -1,26 +1,41 @@
 import java.util.Scanner;
 
 // Using scanner to get values from a user
+// nextLine() to scan for String value
+// nextInt() to scan for int value
+// nextDouble() to scan for a double value
+
 public class Survey {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome. Thank you for taking the survey");
 
-        System.out.println("What is your name?");
+        int counter = 0;
+        System.out.println("\nWhat is your name?");
         String name = scan.nextLine();
+        counter++;
 
-        System.out.println("How much money do you spend on coffee?");
+        System.out.println("\nHow much money do you spend on coffee?");
         double coffeePrice = scan.nextDouble();
+        counter++;
 
-        System.out.println("How much money do you spend on fast food?");
+        System.out.println("\nHow much money do you spend on fast food?");
         double foodPrice = scan.nextDouble();
+        counter++;
 
-        System.out.println("How many times a week do you buy coffee?");
+        System.out.println("\nHow many times a week do you buy coffee?");
         int coffeeAmount = scan.nextInt();
+        counter++;
 
-        System.out.println("How many times a week do you buy food?");
+        System.out.println("\nHow many times a week do you buy food?");
         int foodAmount = scan.nextInt();
+        counter++;
 
+        System.out.println("\nThank you " + name + " for answering all " + counter + " questions");
+        System.out.println("Your fast food expenses are " + (foodPrice / coffeePrice) + " times more than your coffee expenses ");
+        System.out.println("Weekly, you spend $" + (coffeeAmount * coffeePrice) + " on coffee");
+        System.out.println("Weekly, you spend $" + (foodAmount * foodPrice) + " on food");
+    
         scan.close();
 
     }
